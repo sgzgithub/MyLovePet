0ad95f732bb4efb38c0ee4cb4f99ca091293a81c
I'M宋国召 <934154367@qq.com>
2018-03-29 15:19:29 +0800

    设置宠物姓名

--- app/src/main/java/com/example/im/mylovepet/Pet_TypeActivity.java
+++ app/src/main/java/com/example/im/mylovepet/Pet_TypeActivity.java
@@ -9,5 +9,7 @@ public class Pet_TypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet__type);
+
+
     }
 }

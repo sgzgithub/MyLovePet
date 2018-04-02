4c6dff708dc114445b215e687d2dc60a9a9be3b1
I'M宋国召 <934154367@qq.com>
2018-04-02 08:38:45 +0800

    设置宠物体重

--- app/src/main/java/com/example/im/mylovepet/Pet_SettingKg.java
+++ app/src/main/java/com/example/im/mylovepet/Pet_SettingKg.java
@@ -9,5 +9,7 @@ public class Pet_SettingKg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet__setting_kg);
+
+
     }
 }

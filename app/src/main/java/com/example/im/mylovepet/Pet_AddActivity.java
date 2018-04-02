8d96bb46a5af3a1594dd94b4c46af50222ed1116
I'M宋国召 <934154367@qq.com>
2018-04-02 12:06:25 +0800

    设置宠物出生时间

--- app/src/main/java/com/example/im/mylovepet/Pet_AddActivity.java
+++ app/src/main/java/com/example/im/mylovepet/Pet_AddActivity.java
@@ -5,12 +5,14 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.percent.PercentRelativeLayout;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -23,12 +25,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 
+import com.bigkoo.pickerview.TimePickerView;
 import com.example.im.mylovepet.dao.MyDbBeanDao;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Date;
 
 import app.MyApplication;
 import bean.MyDbBean;
@@ -103,7 +108,38 @@ public class Pet_AddActivity extends AppCompatActivity implements View.OnClickLi
                 pw.showAtLocation(findViewById(R.id.popup), Gravity.BOTTOM | Gravity.CENTER, 0, 0);
                 break;
             case R.id.Pet_Date:
+                TimePickerView pvTime = new TimePickerView.Builder(Pet_AddActivity.this, new TimePickerView.OnTimeSelectListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
+                    @Override
+                    public void onTimeSelect(Date date2, View v) {//选中事件回调
+//                        String time = getTime(date2);
+//                        button3.setText(time);
 
+                    }
+                })
+                        .setType(TimePickerView.Type.YEAR_MONTH_DAY)//默认全部显示
+                        .setCancelText("取消")//取消按钮文字
+                        .setSubmitText("确定")//确认按钮文字
+                        .setContentSize(20)//滚轮文字大小
+                        .setTitleSize(20)//标题文字大小
+//                        .setTitleText("请选择时间")//标题文字
+                        .setOutSideCancelable(true)//点击屏幕，点在控件外部范围时，是否取消显示
+                        .isCyclic(true)//是否循环滚动
+                        .setTextColorCenter(Color.BLACK)//设置选中项的颜色
+                        .setTitleColor(Color.BLACK)//标题文字颜色
+                        .setSubmitColor(Color.BLUE)//确定按钮文字颜色
+                        .setCancelColor(Color.BLUE)//取消按钮文字颜色
+//                        .setTitleBgColor(0xFF666666)//标题背景颜色 Night mode
+//                        .setBgColor(0xFF333333)//滚轮背景颜色 Night mode
+//                        .setRange(calendar.get(Calendar.YEAR) - 20, calendar.get(Calendar.YEAR) + 20)//默认是1900-2100年
+//                        .setDate(selectedDate)// 如果不设置的话，默认是系统时间*/
+//                        .setRangDate(startDate,endDate)//起始终止年月日设定
+//                        .setLabel("年","月","日","时","分","秒")
+                        .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+//                        .isDialog(true)//是否显示为对话框样式
+                        .build();
+                pvTime.setDate(Calendar.getInstance());//注：根据需求来决定是否使用该方法（一般是精确到秒的情况），此项可以在弹出选择器的时候重新设置当前时间，避免在初始化之后由于时间已经设定，导致选中时间与当前时间不匹配的问题。
+                pvTime.show();
 
                 break;
             case R.id.Pet_Kg:
@@ -262,4 +298,10 @@ public class Pet_AddActivity extends AppCompatActivity implements View.OnClickLi
         intent.putExtra("noFaceDetection", true);
         startActivityForResult(intent, PHOTO_CLIP);
     }
+
+//
+//    public String getTime(Date date) {//可根据需要自行截取数据显示
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日");
+//        return format.format(date);
+//    }
 }
--- app/src/main/java/com/example/im/mylovepet/Pet_SettingKg.java
+++ app/src/main/java/com/example/im/mylovepet/Pet_SettingKg.java
@@ -1,15 +1,50 @@
 package com.example.im.mylovepet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.im.mylovepet.dao.MyDbBeanDao;
+
+import app.MyApplication;
+import bean.MyDbBean;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Pet_SettingKg extends AppCompatActivity {
+
+    @Bind(R.id.Pet_Add_Kg_exit)
+    ImageView PetAddKgExit;
+    @Bind(R.id.Pet_SettingKg_save)
+    TextView PetSettingKgSave;
+    @Bind(R.id.Pet_SettingKg)
+    EditText PetSettingKg;
+    private MyDbBeanDao beanDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet__setting_kg);
+        ButterKnife.bind(this);
+        beanDao = MyApplication.getApplication().getDaoSession().getMyDbBeanDao();
+    }
 
-
+    @OnClick({R.id.Pet_Add_Kg_exit, R.id.Pet_SettingKg_save})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.Pet_Add_Kg_exit:
+                finish();
+                break;
+            case R.id.Pet_SettingKg_save:
+                MyDbBean dbBean = new MyDbBean();
+                dbBean.setKg(PetSettingKg.getText().toString().trim());
+                beanDao.insert(dbBean);
+                finish();
+                break;
+        }
     }
 }
